e90608df2a9b6e9b01512ab2e01f6ed42b4e9eaf
Muhammad <mohmed6535@gmail.com>
2021-01-03 18:50:42 +0200

    a

---
+++ javascript/01-variables/02-objects/Muhammad Mhameed/script.js
@@ -0,0 +1,18 @@
+// set an object about your family (baba, ab, yaba, aum, yama), add it to array, am's familiy chal's family
+
+// const [yourObj, amObj, chalObj]
+
+const fstFam = {
+  Father: "Naeel",
+  Mother: "Ayat",
+  brother: "borhan",
+  Me: "Muhammad",
+  sister: "teba",
+  sister: "bsmla",
+};
+const amFam = { 1: "zoher", 2: "Naeel", 3: "dlal" };
+const khalFam = { 1: "Khaled", 2: "Muhammad", 3: "Eman" };
+
+const myFamily = [fstFam, amFam, khalFam];
+
+console.log(myFamily);
