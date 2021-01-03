3eb6357bbe00e54a1471c9117deccd7e381c9aae
Muhammad <mohmed6535@gmail.com>
2021-01-03 20:46:53 +0200

    Muhammad Inint

--- javascript/01-variables/02-objects/Muhammad Mhameed/script.js
+++ javascript/01-variables/02-objects/Muhammad Mhameed/script.js
@@ -3,16 +3,32 @@
 // const [yourObj, amObj, chalObj]
 
 const fstFam = {
-  Father: "Naeel",
-  Mother: "Ayat",
-  brother: "borhan",
-  Me: "Muhammad",
-  sister: "teba",
-  sister: "bsmla",
+  Father: { age: 54, name: "Naeel" },
+  Mother: { age: 51, name: "Ayat" },
+  brother: { age: 26, name: "Borhan" },
+  Me: { age: 24, name: "Muhammad" },
+  sister: { age: 19, name: "Teba" },
+  sister: { age: 17, name: "bsmla" },
 };
-const amFam = { 1: "zoher", 2: "Naeel", 3: "dlal" };
-const khalFam = { 1: "Khaled", 2: "Muhammad", 3: "Eman" };
+const amFam = {
+  1: { age: 56, name: "zoher" },
+  2: { age: 54, name: "Naeel" },
+  3: { age: 52, name: "dlal" },
+};
+const khalFam = {
+  1: { age: 52, name: "Khaled" },
+  2: { age: 50, name: "muhammad" },
+  3: { age: 43, name: "eman" },
+};
 
 const myFamily = [fstFam, amFam, khalFam];
+let count = 0;
+let sum = 0;
+myFamily.forEach(function (el) {
+  for (let i in el) {
+    count++;
+    sum += el[i].age;
+  }
+});
 
-console.log(myFamily);
+console.log(sum / count);
