dd65328810cac7d92aea65ffbc29c79e3f1d7d6d
Semat <s.igbaria@gmail.com>
2021-01-10 20:06:04 +0200

    re

--- React/01-basic/example/Semat/src/App.js
+++ React/01-basic/example/Semat/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+ import vil from './mua.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={vil} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Semat Igbaria
         </a>
       </header>
     </div>
