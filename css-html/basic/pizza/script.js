9773d12b045aec4fc2cdb2aa539fabac8f6c3a8e
Rwed <rwedkabha1@gmail.com>
2021-01-10 17:41:04 +0200

    Rwed and Amed pizza order

--- css-html/basic/pizza/script.js
+++ css-html/basic/pizza/script.js
@@ -1,24 +1,30 @@
 let order = document.querySelector(".order");
 let name = document.getElementById('name');
 let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+
 let olives = document.getElementById('olive');
 let mashrom = document.getElementById('mashrom');
 let cheese = document.getElementById('cheese');
 var add = "";
 order.addEventListener("submit", (e) => {
     e.preventDefault();
-
+add="";
+if(name != "" && email != "" && phone != "" ) {
     if (olives.checked) {
-        add = add +  "olives punus 15%";
+        add = add +  " olives punus 15%";
     }
     if (mashrom.checked) {
-        add = add + "mashroum";
+        add = add + " and mashroum";
     }
     if (cheese.checked) {
-        add =add +  "cheese";
+        add =add +  "and  cheese";
     }
+}else {
+    alert("Please fill the info")
+}
     
-    alert("Your pizza have additionals = " +add);
+    alert("Hi Dear, " +name.value +" " + "Your pizza have additionals = " +add);
 });
 
 
