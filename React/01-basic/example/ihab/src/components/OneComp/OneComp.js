04c2981d54eb3cb2f139151ccbb7ddb277c4da1c
Ihab <Ihab.ah1@gmail.com>
2021-01-12 21:32:47 +0200

    react - ihab

---
+++ React/01-basic/example/ihab/src/components/OneComp/OneComp.js
@@ -0,0 +1,13 @@
+import './Card.css'
+import React from "react"
+
+function OneComp(props) {
+    return (
+        <div className='card'>
+           
+        </div>
+    )
+}
+
+export default Card
+
---
+++ React/01-basic/example/ihab/src/components/ThreeComp/ThreeComp.js
@@ -0,0 +1,13 @@
+import './Card.css'
+import React from "react"
+
+function ThreeComp(props) {
+    return (
+        <div className='card'>
+           
+        </div>
+    )
+}
+
+export default Card
+
