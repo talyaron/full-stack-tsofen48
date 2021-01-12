396cda2321fd88a95769db6b3e5ba098cd87378b
mhmd kilaney <mhmdmhmood1937@gmail.com>
2021-01-12 23:25:22 +0200

    Mohamad kilaney

---
+++ React/01-basic/example/Mohamad kilaney/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import photo from './m.jpg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={photo} className="App-logo" alt="logo" />
+          <h2>Mohamad kilaney</h2>
+          <p>
+            Mohamad Mohamad Mohamad Mohamad Mohamad
+          </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
