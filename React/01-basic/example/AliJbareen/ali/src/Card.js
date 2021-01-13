a42fc532f2cebc38f1c8631b615f32df96dcb4ab
Tal <tal.yaron@gmail.com>
2021-01-13 19:27:58 +0200

    map

--- React/01-basic/example/AliJbareen/ali/src/Card.js
+++ React/01-basic/example/AliJbareen/ali/src/Card.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Card, Icon } from 'semantic-ui-react'
 
-const profileCard = (data) => (
-  <Card
-    image={data.img}
-    header={data.name}
-    meta={data.meta}
-    description={data.desc}
-    extra={data.extra}
+const profileCard = (props) => {
+
+  const { img, name, meta, desc, extra } = props;
+  
+  return <Card
+    image={img}
+    header={name}
+    meta={meta}
+    description={desc}
+    extra={extra}
   />
-)
+}
 
 export default profileCard
