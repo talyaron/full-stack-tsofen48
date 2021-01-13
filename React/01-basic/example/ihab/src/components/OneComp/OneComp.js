84738b375dd5421ef3e964fb490fed95f93c7e04
Ihab <Ihab.ah1@gmail.com>
2021-01-13 16:04:11 +0200

    Uml+react /ihab

--- React/01-basic/example/ihab/src/components/OneComp/OneComp.js
+++ React/01-basic/example/ihab/src/components/OneComp/OneComp.js
@@ -3,7 +3,7 @@ import React from "react"
 
 function OneComp(props) {
     return (
-        <div className='card'>
+        <div className='OneComp'>
            <h1>{props.nameOf}</h1>
         <p>{props.title}</p>
         <img alt="" src={props.img} />
