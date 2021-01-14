72e371af731dee528ef251b05ac8eab4628ef46e
Ihab <Ihab.ah1@gmail.com>
2021-01-14 22:16:01 +0200

    v15

--- React/01-basic/example/ihab/src/components/TwoComp/TwoComp.js
+++ React/01-basic/example/ihab/src/components/TwoComp/TwoComp.js
@@ -1,15 +1,27 @@
 import './TwoComp.css'
-import React from "react"
+import React,{useState} from "react"
 
 function TwoComp(props) {
+
+    const [Counter, setCounter] = useState(0); 
+
+    function newHandler(e){
+      let NewCounter = Counter+1;
+      setCounter(NewCounter); 
+     // console.log(NewCounter); 
+    }
+
     return (
         <div className='TwoComp'>
         <h1>{props.nameOf}</h1>
         <p>{props.title}</p>
         <img alt="" src={props.img} />
+        <button onClick={newHandler}>Number of Click {Counter}</button>
         </div>
     )
 }
+
+
 
 export default TwoComp
 
