81993d29eca97a269749072c0fcf43c0862253c3
Hosen Agbaria <50136716+HosenAgbaria@users.noreply.github.com>
2021-01-15 10:56:07 +0200

    merge remove-tracking

--- React/03-conditionalRendering/Mosaab/src/index.js
+++ React/03-conditionalRendering/Mosaab/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Goal from './components/Goal'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Goal />
   </React.StrictMode>,
   document.getElementById('root')
 );
