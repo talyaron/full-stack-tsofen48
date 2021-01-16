96acd42fda1b3e09d88aa190d177bbc243185af9
rema <remawail06@gmail.com>
2021-01-16 15:06:21 +0200

    --

--- React/04-Router/rema/src/App.js
+++ React/04-Router/rema/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+
+import React, { useState } from 'react';
+import'./App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,19 +9,24 @@ import {
 } from "react-router-dom";
 
 export default function App() {
-  return (
+  const [pressed, setPressed] = useState(false);
+  function changeBorderColor() {
+    setPressed(!pressed)
+  }
+  return (<div className='container'>
+    <div className='header'><div className='imgContainer'><img className='profileImg' src='https://photof8.com/wp-content/uploads/2016/10/FutureAst007-2.jpg'></img><h3>עיסאווי פריג</h3></div></div>
     <Router>
       <div>
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link onClick={changeBorderColor} className={pressed ? 'pressed-a' : 'not-pressed-a'} to="/details">סיכום פרטים</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/extraDetails">עוד מידע</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/vote">ניתוני הצבעה</Link>
             </li>
           </ul>
         </nav>
@@ -27,29 +34,40 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/extraDetails">
+            <ExtraDetails />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/vote">
+            <Vote />
           </Route>
-          <Route path="/">
-            <Home />
+          <Route path="/details">
+            <Details />
           </Route>
         </Switch>
       </div>
     </Router>
-  );
+    </div> );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+function Details() {
+  return <div className='textContainer'><div className='paragraph-1'><h4>נסיון תעסוקתי</h4><p>מנכ"ל פוקס</p>
+   <div className='button-container-row-1'><button><span>בהווה</span></button></div><p>
+    דירקטור קו השקעות</p>
+    <div className='button-container-row-2'><button><span>בהווה</span></button></div>
+    <p>סמנכ"ל כספים מחסני חשמל</p><p> אלוף משנה 8200</p></div>
+    <div className='paragraph-2'><h4>מומחיות פיננסית</h4><p>יש מומחיות</p></div>
+    <div className='paragraph-3'><h4>השכלה</h4><p>לימודי משפטים, אוניברסיטת בר אילן</p><p>קורס ברמנים, בר מאסטר</p><p>לימודי דת ורוח, האוניברסיטה הפתוחה</p></div>
+    <div className='paragraph-4'><h4>ניגוד עניינים</h4><p>אין</p></div>
+    <div className='paragraph-5'><h4>קרוב משפחה של בעל שליטה</h4><p>בן דוד של מנכ"ל הראל פיננסים</p></div>
+    <div className='linkedin-buttonContainer'><button><span>צפייה בנתונים בלינקדאין</span></button></div>
+    {/* <div className='buttons-container'><button>בהווה</button><button>בהווה</button></div> */}
+    </div>;
 }
 
-function About() {
-  return <h2>About</h2>;
+function ExtraDetails() {
+  return <h2></h2>;
 }
 
-function Users() {
-  return <div> <h2>Users</h2>  <img src='https://cdn.iconscout.com/icon/premium/png-512-thumb/hello-5-578098.png'></img></div>;
+function Vote() {
+  return <h2></h2>;
 }
