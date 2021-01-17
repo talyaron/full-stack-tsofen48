03d4323889bb4ac6d139c2edf13cf2efa46c3045
Ihab <Ihab.ah1@gmail.com>
2021-01-17 02:34:14 +0200

    update

--- React/02-map/ihab/src/App.js
+++ React/02-map/ihab/src/App.js
@@ -1,83 +1,56 @@
-import React, {Component} from 'react'; 
-import './App1.css'; 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			newItem:"",
-			list:[]
-		}
-	}
-
-	updateInput(key, value){
-		this.setState({
-           [key]: value
-		});
-
-	}
-
-	addItem() {
-		const newItem={
-			id: 1+ Math.random(),
-			value: this.state.newItem.slice()
-		
-		};
-
-		// list item
-		const list = [...this.state.list];
-
-		list.push(newItem);
-
-		this.setState({
-			list,
-			newItem:""
-		
-		});
-	}
-	
-deleteItem(id){
-	const list = [...this.state.list];
-
-	const updatedList = list.filter(item => item.id !== id);
-
-	this.setState({list: updatedList});
-}
-
-	render() {
-		return (
-		<div className="App">
+export default function App() {
+  return (
+    <Router>
       <div>
-			<h2> Ihab To Do List</h2>
-			 <br/>
-			 <input
-			 type="text"
-			 placeholder="Type item here.."
-			 value={this.state.newItem}
-			 onChange={e => this.updateInput("newItem", e.target.value)}
-			 />
-			 <button
-			 onClick={() => this.addItem()}
-			 >
-				 Add
-			 </button>
-			 <br/>
-			 <ul>
-				 {this.state.list.map(item => {
-					 return(
-						 <li key={item.id}>
-						  
-						 <button class="btnDlt" onClick={() => this.deleteItem(item.id)}>
-							 {item.value}   [X]				
-						 </button>
-						 </li>
-					 )
-				 })}
-			 </ul>
-		 </div>
-		</div>
-		);
-	}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App; 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
