8b3df3f560e393c5416852aad35d52b711d261f9
HeBa <heba.mah93@gmail.com>
2021-01-18 22:55:35 +0200

    heba-mohamad

---
+++ Projects/OpenPensia/heba/src/components/Head.js
@@ -0,0 +1,20 @@
+
+import './Head.css';
+import heba from './Screenshot_20181030-223624.jpg'
+
+function Head(){
+
+    
+    return (
+            <div className="container">
+                 <h> פנק הפועלים</h>
+                 <img src={heba} alt="heba"/>
+                 
+                 
+
+            </div>
+    );
+
+}
+
+export default Head
