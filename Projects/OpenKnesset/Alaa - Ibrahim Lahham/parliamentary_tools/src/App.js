0de3ecbfbc23a62fafdac5945fe120ce0882bf51
Alaa-S-M <alaasaabni@gmail.com>
2021-01-19 15:52:43 +0200

    alaa - keneset update

--- Projects/OpenKnesset/Alaa - Ibrahim Lahham/parliamentary_tools/src/App.js
+++ Projects/OpenKnesset/Alaa - Ibrahim Lahham/parliamentary_tools/src/App.js
@@ -5,6 +5,8 @@ import React from 'react';
 import Cards from './components/Cards.js';
 
 const tools = getCardsFromDataBase();
+let currentTabName = 'כלים פרלמנטריים';
+let userType = 'לוח מעקב';
 
 function App() {
   return (
@@ -12,16 +14,28 @@ function App() {
       <header id='app-header' class='container App-header'>
         <div class='row'>
           <div id='logo' class='span4 clearfix'>
-            <h1><a href='/'><img src='https://oknesset.org/static/img/oknesset-logo.png' alt='oknesset-logo' /><span>כנסת פתוחה</span></a></h1>
+            <h1><a href='https://oknesset.org/'><img src='https://oknesset.org/static/img/oknesset-logo.png' alt='oknesset-logo' /><span>כנסת פתוחה</span></a></h1>
           </div>
         </div>
 
         <ul class='nav nav-pills'>
           <li id='nav-parties'><a href='/members/index.html'>ח'כים וסיעות</a></li>
           <li id='nav-committees'><a href='/committees/index.html'>ועדות</a></li>
+          <li id='nav-committees'><a href='/committees/index.html'>כלים פרלמנטריים</a></li>
         </ul>
       </header>
+      <section className='secondary-nav-bar'>
+        <div className='path'>
+          דף הבית /
+            <span className='current-tab-name'> {currentTabName} </span>
+        </div>
 
+        <div className='user-links'>
+          <a className='disconnect' href='#'> התנתק </a>
+            |
+          <a className='link'> {userType} </a>
+        </div>
+      </section>
       <Cards tools={tools} />
 
     </div>
