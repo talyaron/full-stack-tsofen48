08f42f360ffcb8b30f9ee5c213ddb08d0c49d0e5
Hosen Agbaria <50136716+HosenAgbaria@users.noreply.github.com>
2021-01-19 16:39:49 +0200

    Update AutoCompleteDropDown.js

--- Projects/OpenKnesset/knest-rema-hosen/src/components/SuggestionFrom/AutoCompleteDropDown/AutoCompleteDropDown.js
+++ Projects/OpenKnesset/knest-rema-hosen/src/components/SuggestionFrom/AutoCompleteDropDown/AutoCompleteDropDown.js
@@ -1,19 +1,11 @@
-// import faker from 'faker'
-// import _ from 'lodash'
-
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import './AutoCompleteDropDown.css'
-// const addressDefinitions = faker.definitions.address
-// const stateOptions = _.map(addressDefinitions.state, (state, index) => ({
-//   key: addressDefinitions.state_abbr[index],
-//   text: state,
-//   value: addressDefinitions.state_abbr[index],
-// }))
+
 
 const AutoCompleteDropDown = () => (
   <Dropdown
-    placeholder='שש'
+    placeholder='הכנשם השם של חבר הכנסת'
     fluid
     multiple
     search
