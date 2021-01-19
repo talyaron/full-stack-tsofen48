3341cff6c8b0338e72c60f8f8c8c98d182d18aa1
Khalil <khaleal@hotmail.com>
2021-01-19 18:29:12 +0200

    khalil open pensia

---
+++ Projects/OpenPensia/khalil/src/components/contents/content.js
@@ -0,0 +1,17 @@
+import './content.css'
+import Block from '../blocks/block'
+
+function content() {
+    return (
+        <div className="content">
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+          <Block title='מי יהיו הדרקטורים בנק הפועלים' status='מחכים לתשובה'/>
+        </div>
+    );
+}
+
+export default content;
