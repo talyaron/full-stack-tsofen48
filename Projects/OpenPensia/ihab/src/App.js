3947f34ff746d8061989107b7911ce86fb2f465d
Rwed <rwedkabha1@gmail.com>
2021-01-20 20:40:03 +0200

    ihab Pensia

--- Projects/OpenPensia/ihab/src/App.js
+++ Projects/OpenPensia/ihab/src/App.js
@@ -9,24 +9,6 @@ import FormControl from 'react-bootstrap/FormControl'
 //components
 import Icon from './components/Icon.js';
 
-const icons = [
-  {
-    title: 'https://www.linkedin.com/in/alaasaabni/',
-    href: 'https://www.linkedin.com/in/alaasaabni/',
-    target: '_blank',
-    // img: '',
-    // innerHTML: 'linkedin'
-  },
-  {
-    title: 'https://github.com/Alaa-S-M',
-    href: 'https://github.com/Alaa-S-M',
-    target: '_blank',
-    // img: '',
-    // innerHTML: 'GitHub'
-  },
-];
-
-const myImgSrc = "https://media-exp1.licdn.com/dms/image/C4E03AQFWZrT9WGRmHA/profile-displayphoto-shrink_400_400/0/1516827527497?e=1616025600&v=beta&t=mppJLKNmcZbNq5TzMGravh3TvnK84LgfqipAdFK2Wgc";
 
 function App() {
   return (
