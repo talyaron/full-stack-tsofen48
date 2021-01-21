74cc2b6fda9c47583d9e8402ce809ea3c35433da
Ibrahim Lahham <abo.la7am.10@gmail.com>
2021-01-21 23:15:31 +0200

    NodeJs (nodemon and html access)

--- nodeJs/basic/ibrahimlahham/server.js
+++ nodeJs/basic/ibrahimlahham/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 
-app.get('/',function (req, res){
-  res.send('Hello World!')
-})
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/main.html'));
+});
+
+
 
 app.get('/home', function(req, res){
     res.send(`Example app `);
