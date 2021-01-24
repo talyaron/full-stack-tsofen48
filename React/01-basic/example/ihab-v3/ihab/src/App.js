b05a38249f0ec8eca1e54f7ef122db5b13efb3e5
Ihab <Ihab.ah1@gmail.com>
2021-01-24 03:03:04 +0200

    update

--- React/01-basic/example/ihab-v3/ihab/src/App.js
+++ React/01-basic/example/ihab-v3/ihab/src/App.js
@@ -6,13 +6,8 @@ import OneComp from './components/OneComp/OneComp'
 
 function App() { //component
  
-  const blocks = [{name: "aa", model:"dasd"},{name:"aa", model:"adsd"}]; 
-  const listblock= blocks.map((block) => 
-  <span>name{block.name} {block.model}</span> 
-  ); 
-
   return (
-    <OneComp />
+    <OneComp/>
   );
 };
 
--- React/01-basic/example/ihab-v3/ihab/src/components/OneComp/OneComp.js
+++ React/01-basic/example/ihab-v3/ihab/src/components/OneComp/OneComp.js
@@ -21,9 +21,7 @@ function OneComp(props) {
   <a id="logoID" className="logo">התחבר למערכת<img id="logo2" src={logo2} alt=""></img></a>
   <div className="header-right">
     <span class="active" href="#">ח"כים וסיעות</span>
-     <span href="#contact">ועדות</span>
-      
-
+    <span href="#contact">ועדות</span>
     <span href="#about">כלים פרלמנטריים</span>
   </div>
 </div>
@@ -76,8 +74,9 @@ function OneComp(props) {
             
               <div className="absolute14"> 
               <img  id ="img4"src={imageAtid} alt=""/> 
-              <p id="sub14"> <br/> {props.listblock.block}
-              </p>
+              <p id="sub14"> <br/> נאומים בני דקה (תיקון מס 112)  
+           51. (א) מתוך דף הפייסבוק של האקדמיה ללשון העברית: המילה סתם מוכרת לנו כבר מן המשנה, למשל אם של עבודה זרה
+            </p>
              </div>
 
           </div>
