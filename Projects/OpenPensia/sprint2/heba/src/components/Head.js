869c8c784f46e8f11ef7eddd9387d163bd4b569a
Ameed <ameedkabha@gmail.com>
2021-01-26 18:14:15 +0200

    p

---
+++ Projects/OpenPensia/sprint2/heba/src/components/Head.js
@@ -0,0 +1,36 @@
+
+import './Head.css';
+import heba from './Screenshot_20181030-223624.jpg'
+
+function Head(){
+
+    
+    return (
+<<<<<<< Updated upstream
+           <div>
+            <div className="container">
+                 <h>  בנק הפועלים</h>
+                 <img src={heba} alt="heba"/>
+                 
+              </div>
+             <div className="topnav">
+             <a className="active" >סיכום פרטים</a>
+             <a className="active-no" href="#news"> עוד מידע</a>
+             <a className="active-no" href="#contact">ניתוני  הצבעה</a>
+              </div>
+              
+              </div> 
+=======
+            <div className="container">
+                 <h> פנק הפועלים</h>
+                 <img src={heba} alt="heba"/>
+                 
+                 
+
+            </div>
+>>>>>>> Stashed changes
+    );
+
+}
+
+export default Head
---
+++ Projects/OpenPensia/sprint2/rwed/src/view/components/Card/group.js
@@ -0,0 +1,9 @@
+import './Card.css'
+function Group() {
+    return (
+        <div className='group'>
+        </div>
+    )
+}
+
+export default Group
