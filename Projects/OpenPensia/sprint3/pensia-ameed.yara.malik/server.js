7ee687a936edb163bbc5d8336bf67927e44413cb
Ameed <ameedkabha@gmail.com>
2021-01-26 21:52:19 +0200

    pensia final- ameed-yara-malik

--- Projects/OpenPensia/sprint3/pensia-ameed.yara.malik/server.js
+++ Projects/OpenPensia/sprint3/pensia-ameed.yara.malik/server.js
@@ -1,11 +1,12 @@
-import './App.css';
-// import React, { useState } from 'react';
-
-//components
-import Screen from './components/screen.js';
+const express=require('express')
+var bodyParser = require('body-parser')
+const app = express();
 
 
-const choises = [
+app.use(express.static('rct/build'));
+app.use(bodyParser.json())
+
+const companies = [
     { logo: 'https://is3-ssl.mzstatic.com/image/thumb/Purple114/v4/7e/d9/c7/7ed9c7ec-7022-b21e-a391-10cfe7cf14ef/source/256x256bb.jpg', name: 'פרשמרקט', par: 'קמעונאות מזון' },
     { logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaBGcvDXYbWULauODr04g1qY0v5TZq1rL_nw&usqp=CAU', name: 'הולמס פלייס', par: 'חדרי כושר' },
     { logo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGkvo1rUEuU8g/company-logo_200_200/0/1519890596281?e=2159024400&v=beta&t=_CUC8VCcYPoTTiFFfVANK3-BrrEDskWGg2tjZPqHFIs', name: 'חילן', par: 'טכנולוגיה' },
@@ -16,30 +17,15 @@ const choises = [
     { logo: 'https://media-exp1.licdn.com/dms/image/C4D0BAQHVAbtCWVaNsw/company-logo_200_200/0/1561277330815?e=2159024400&v=beta&t=QpnVDY0nfRwTvbnKp3ZSuZ9os0vQEIRLouSOf3TPT-E', name: 'בנק הפועלים', par: 'בנקאות' }
   ]
 
+  app.get('/get-companieslist',(req, res)=>{
+    res.send(companies)
+  })
 
-function App() {
 
-  return (
+const port = process.env.PORT || 3000;
 
-    <div className="App">
-      <header>
+app.listen(port,function(){
+    console.log('listening',port)
+})
 
-      </header>
 
-      {choises.map((choise, index) => {
-           return (<Screen
-            key={index}
-            logo={choise.logo}
-            name={choise.name}
-            par={choise.par}
-          />)
-        })
-        }
-
-    </div>
-
-    
-  );
-}
-
-export default App;
