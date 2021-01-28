d48443b67c51f4218dd79dded6e9a6ede21c66d3
remaegbaria <76660321+remaegbaria@users.noreply.github.com>
2021-01-28 21:35:01 +0200

    Add files via upload

---
+++ Projects/OpenPensia/sprint3/pensia-rema-hosen-front-End/src/components/ExtraDetails/ExtraDetails.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from "react";
+import "./ExtraDetails.css";
+import Article from "./Article/Article";
+export default function ExtraDetails(props) {
+  const [articlesDetails, setArticlesDetails] = useState([]);
+  const [showMoreInfoBtn, setShowMoreInfoBtn] = useState(false)
+  useEffect(() => {
+
+    
+    fetch('https://pensia-rema-hosen-backend.herokuapp.com/').then(response=>{
+    
+    return response.json()}).then(res=>{
+
+      setArticlesDetails(res);
+     
+    }) 
+    fetch(`http://localhost:3000/${window.location.search}`).then(response=>{
+      return response.json()}).then(res=>{
+    
+       setShowMoreInfoBtn(res.isAdmin);
+   
+    }) 
+
+  }, [])
+ 
+
+  return (
+    <div className="extraDetails">
+      <div className="moreInfoBtnContainer" style={{display:showMoreInfoBtn?'flex' : 'none'}}>
+        <button className="moreInfoBtn">יש לך מידע? לחצ/י כאן</button>
+      </div>
+      {articlesDetails.map((articleDetails, index) =>{
+        return( <Article key ={index} articleSrc={articleDetails.articleSource} articleSumUp={articleDetails.articleSubTitle} articleLink={articleDetails.linkToArticle} />);
+      })}
+   
+    </div>
+  );
+}
