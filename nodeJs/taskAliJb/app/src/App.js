4f37e66e3ae74b53e9cbe30d814518807aea1293
Ali Jbareen <alijb@post.bgu.ac.il>
2021-01-29 12:16:52 +0200

    taskfix

--- nodeJs/taskAliJb/app/src/App.js
+++ nodeJs/taskAliJb/app/src/App.js
@@ -1,26 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+// import { ADD_TODO } from './redux/actionTypes';
+import { addTodo } from './redux/actions';
+import { useState } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+//components
+import Cards from './components/Cards/Cards';
+
 
 
 function App() {
 
-  const hitBackend = () => {
-    axios.get('/testAPI')
-    .then((response) => {
-    console.log(response.data)
-    })
+  const [taskTxt, setTaskTxt] = useState('')
+  const [apiResponse,setapiResponse] =useState("empty");
+  const dispatch = useDispatch();
+  
+ 
+  const state = useSelector(state=>state);
+  console.log(state)
+
+
+  function handleAdd(e) {
+    e.preventDefault();
+    dispatch(addTodo(taskTxt));
+    setTaskTxt('');
+    e.target.reset();
+  }
+
+  function handleInput(e) {
+    setTaskTxt(e.target.value)
+  }
+
+function callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => setapiResponse(res));
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        <button onClick={hitBackend}>Send request</button>
-        </p>
-      </header>
+      <form onSubmit={handleAdd}>
+        <button type='submit'>Add Task</button>
+        <input type='text' placeholder='משימה' defaultValue={taskTxt} onKeyUp={handleInput} />
+      </form>
+
+      <Cards />
+
+      <button onClick={callAPI}>
+  call api
+</button>
+    <p>{apiResponse}</p>
     </div>
   );
 }
 
 export default App;
+
