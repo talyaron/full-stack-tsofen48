b1b0b74b6b5ee6670fa295b4ad1dd8b5bb29b3bf
Alaa-S-M <alaasaabni@gmail.com>
2021-01-29 15:30:52 +0200

    alaa

--- Exams/Exam1a/alaa/server.js
+++ Exams/Exam1a/alaa/server.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const app = express();
 var bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+//helpping functions
+function generateRandomId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+  // return 1;
+}
 
-app.post("/", (req, res) => {
-  res.send(users);
-});
-app.get("/", (req, res) => {
-  console.log("test");
-});
+const gifts = [
+  { id: generateRandomId(), price: 78, src: 'https://image.freepik.com/free-photo/white-gift-box-with-red-ribbon-bow-white-background_1357-337.jpg' },
+  { id: generateRandomId(), price: 55, src: 'https://image.freepik.com/free-photo/white-gift-box-with-red-ribbon-bow-white-background_1357-337.jpg' },
+  { id: generateRandomId(), price: 20, src: 'https://image.freepik.com/free-photo/white-gift-box-with-red-ribbon-bow-white-background_1357-337.jpg' },
+  { id: generateRandomId(), price: 39, src: 'https://image.freepik.com/free-photo/white-gift-box-with-red-ribbon-bow-white-background_1357-337.jpg' },
+  { id: generateRandomId(), price: 5, src: 'https://image.freepik.com/free-photo/white-gift-box-with-red-ribbon-bow-white-background_1357-337.jpg' },
+];
+
+console.log('server ')
+app.get('/gifts', (req, res) => {
+  console.log('server gifts')
+  res.send(gifts);
+})
 
 // Server run
 const port = process.env.PORT || 3000;
