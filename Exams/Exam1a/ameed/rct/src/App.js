d5a25bd3a3f230fc666676ad3582cf2bdffd2b06
Ameed <ameedkabha@gmail.com>
2021-01-29 15:29:38 +0200

    ameed exam

--- Exams/Exam1a/ameed/rct/src/App.js
+++ Exams/Exam1a/ameed/rct/src/App.js
@@ -2,15 +2,57 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 
+//components
+import Gifts from './components/gifts';
+
+
 
 function App() {
 
+  const [gifts, setGifts] = useState([]);
+
+  useEffect(() => {
+    fetch('/get-gifts')
+      .then(r => r.json())
+      .then(data => {
+        console.log(data)
+        setGifts(data);
+      })
+  }, [])
+
   
+
+
+  fetch('/get-totalprice')
+  .then(r=>r.json())
+  .then(data=>{
+      console.log(data)
+  })
+
+
   return (
 
     <div className="App">
+    
+    
+         
+        
+      <div>
 
-     
+        {gifts.map((list, index) => {
+          return (
+            
+          <Gifts
+            key={index}
+            price={list.price}
+            logo={list.logo}
+            button={list.button}
+          />)
+
+
+        })}
+
+      </div>
 
 
 
--- Exams/Exam1a/ameed/server.js
+++ Exams/Exam1a/ameed/server.js
@@ -2,9 +2,29 @@ const express=require('express')
 var bodyParser = require('body-parser')
 const app = express();
 
+var cookieParser = require('cookie-parser');
+app.use(cookieParser())
 
 app.use(express.static('rct/build'));
 app.use(bodyParser.json())
+
+
+const gifts = [
+    {id:1 , logo: 'https://img.favpng.com/24/17/21/gift-card-business-christmas-gift-png-favpng-jubZcNtC9eD1CfFbTByYY0RX1.jpg', price: 45, button: 'add' },
+    {id:2 , logo: 'https://img.favpng.com/24/17/21/gift-card-business-christmas-gift-png-favpng-jubZcNtC9eD1CfFbTByYY0RX1.jpg', price: 40 , button: 'add' },
+    {id:3 , logo: 'https://img.favpng.com/24/17/21/gift-card-business-christmas-gift-png-favpng-jubZcNtC9eD1CfFbTByYY0RX1.jpg', price: 30, button: 'add' },
+    {id:4 , logo: 'https://img.favpng.com/24/17/21/gift-card-business-christmas-gift-png-favpng-jubZcNtC9eD1CfFbTByYY0RX1.jpg', price: 40, button: 'add' }
+   
+  ]
+  let totoalPrice= 'total sum available : ' + 100 + '$';
+
+  app.get('/get-gifts',(req, res,next)=>{
+    res.send(gifts);
+    next();
+  })
+  app.get('/get-totalprice',(req, res)=>{
+    res.send(totoalPrice);
+  })
 
 
 const port = process.env.PORT || 3000;
