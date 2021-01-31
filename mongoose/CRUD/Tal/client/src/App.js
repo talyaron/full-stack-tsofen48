2be7cf527cf867acdf46c46da948cb8f40661c3f
Tal <tal.yaron@gmail.com>
2021-01-31 17:38:56 +0200

    inbit

--- mongoose/CRUD/Tal/client/src/App.js
+++ mongoose/CRUD/Tal/client/src/App.js
@@ -1,41 +1,16 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 
 //import components
-import Header from './components/header/header';
-import Row from './components/rows/row';
 
 
 function App() {
 
-  const [amount, setAmount] = useState(0);
-  const [gift, setGift] = useState([]);
-
-  useEffect(() => {
-    fetch('/giftAmount')
-      .then(r => r.json())
-      .then(data => {
-        console.log(data)
-        setAmount(data.amount)
-        setGift(data.gift)
-      })
-  }, [])
-
+  
   return (
     <div className="App">
-      <Header Amount={amount} />
-      <div className="Content">
-        {gift.map((row, index) => {
-          return (<Row price={row.price} imgSrc={row.imgSrc} key={row.id} id={row.id} />)
-        })}
-      </div>
-
-      <footer>
-        <button>
-          add to cart
-      </button>
-      </footer>
+      hi
     </div>
   );
 }
