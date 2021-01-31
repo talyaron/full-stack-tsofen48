efbff5b4a292a7829a49c13f09714c56890aa83c
ibk <ekittani@gmail.com>
2021-01-31 21:02:50 +0200

    ibk crud final

---
+++ mongoose/CRUD/ibk final/client/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, { useEffect, useState} from 'react';
+
+
+//import components
+import Kitten from './components/Kitten/Kitten';
+
+function App() {
+
+  const [kittens, setKittens] = useState([]);
+
+  useEffect(()=>{
+    fetch('/get-kittens')
+    .then(r=>r.json())
+    .then(data=>setKittens(data.kittens))
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const name = e.target.children.name.value;
+    e.target.children.name.value = '';
+
+    fetch('/send-kitten-name',{
+      method:'post',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({name})
+    }).then(r=>r.json())
+    .then(data=>console.log(data))
+  }
+  
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='kitten name' name='name'></input>
+        {kittens.map((kitten, index)=>{
+          return(<Kitten key={index} kitten={kitten}></Kitten>)
+        })
+        }
+      </form>
+    </div>
+  );
+}
+
+export default App;
