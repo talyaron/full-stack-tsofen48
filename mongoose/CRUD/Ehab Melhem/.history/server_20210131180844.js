84a4c1665d565f296633b8a6c949ac326f8d41c8
ehab <Melhemehab@gmail.com>
2021-01-31 18:18:11 +0200

    update

---
+++ mongoose/CRUD/Ehab Melhem/.history/server_20210131180844.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+mongoose.connect(
+  "+srv://ehab:e0iVP2mdpxBvW2OZ@cluster0.ojjnq.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const Cat = mongoose.model("Cat", { name: String, souls: Number }); //schema
+
+const kitty = new Cat({ name: "Kitti", souls: 9 }); //instance
+
+kitty.save().then(() => console.log("kitti was saved to DB")); //save
+
+app.listen(3000, () => {
+  console.log("listen on port 3000");
+});
