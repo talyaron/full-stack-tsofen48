7b21c75d6c9d79ded5576f36d46723bb2ff350d7
ehab <Melhemehab@gmail.com>
2021-02-02 22:14:37 +0200

    update

---
+++ Projects/OpenPensia/sprint4/EhabMelhem-ibrahimKitanny/ehabmelhem_ibrahimktani/ehab/.history/src/App_20210119223933.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./Compents/Header";
+import Information from "./Compents/Information";
+import Navbar from "./Compents/Navbar";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+function App() {
+  return (
+    <div className="app">
+      <div className="contaner">
+        <Header />
+        <Navbar />
+        <Information />
+        <p>מה הבחינה שלך</p>
+        <ThumbUpIcon />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Projects/OpenPensia/sprint4/EhabMelhem-ibrahimKitanny/ehabmelhem_ibrahimktani/ehab/.history/src/Compents/Header_20210119225957.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import "./Header.css";
+function Header({ company, Icon, name }) {
+  return (
+    <div className="header">
+      <div className="header__text">
+        <h4>{company}</h4>
+        <ChevronRightIcon />
+      </div>
+      <Avatar className="avatar" src={Icon} alt="Ehab" />
+      <h4>איהאב מלחם</h4>
+    </div>
+  );
+}
+
+export default Header;
