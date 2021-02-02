7177214b1b8a4cd758a4ce24c093eeb830af9b66
Alaa-S-M <alaasaabni@gmail.com>
2021-02-02 09:59:55 +0200

    added server side to Pensia projects. updated json files

---
+++ Projects/OpenPensia/sprint4/Alaa - Ibrahim Lahham/more_info/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+});
