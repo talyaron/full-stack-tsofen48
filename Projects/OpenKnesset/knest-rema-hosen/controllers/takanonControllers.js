3587053378a7f1d60a008b763dba8dfd90b95a7d
rema <remawail06@gmail.com>
2021-02-03 14:24:31 +0200

    updateKnset

--- Projects/OpenKnesset/knest-rema-hosen/controllers/takanonControllers.js
+++ Projects/OpenKnesset/knest-rema-hosen/controllers/takanonControllers.js
@@ -1,8 +1,35 @@
-let TAKANON={takanon:'כינוס הכנסת בתקופת הפוגרה(תיקונים מספר 107,121 ו-128)',text:'21. (א)  25 חברי הכנסת הדורשים לכנס את הכנסת בתקופת הפגרה , בהתאם לסעיף 9(ב) לחוק הכנסת , רשאים לפרט בדרישתם נושא אחד או שני נושאים ; יושב ראש הכנסת יעמיד את ..'};
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://rema06:6m30hsL0v0jsdnXh@cluster0.eua4t.mongodb.net/open-knest', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',  ()=> {
+   console.log('we are connected to DB')
+});
+
+
+const TakanonSchema = new mongoose.Schema({
+    takanon: String,
+    text: String
+  }); 
+  
+  const takanon = mongoose.model('TakanonSchema', TakanonSchema); 
+
+  // const takanonItem = new takanon({ takanon: 'כינוס הכנסת בתקופת הפוגרה(תיקונים מספר 107,121 ו-128)',text:'21. (א)  25 חברי הכנסת הדורשים לכנס את הכנסת בתקופת הפגרה , בהתאם לסעיף 9(ב) לחוק הכנסת , רשאים לפרט בדרישתם נושא אחד או שני נושאים ; יושב ראש הכנסת יעמיד את ..'});
+  // takanonItem.save().then(()=>console.log('saved to DB'));
+
+// let TAKANON={takanon:'כינוס הכנסת בתקופת הפוגרה(תיקונים מספר 107,121 ו-128)',text:'21. (א)  25 חברי הכנסת הדורשים לכנס את הכנסת בתקופת הפגרה , בהתאם לסעיף 9(ב) לחוק הכנסת , רשאים לפרט בדרישתם נושא אחד או שני נושאים ; יושב ראש הכנסת יעמיד את ..'};
 
 // handlers
 exports.getTAKANON = (req, res) => {
-    res.send({ TAKANON: TAKANON});
+  try {
+    //get from DB
+    takanon.find({}).then((TAKANON) => {
+      res.send({ TAKANON: TAKANON});
+    });
+  } catch (e) {
+    res.send({ error: e });
+  }
    };
 
   exports.getTAKANONById = (req, res) => {
--- Projects/OpenKnesset/knest-rema-hosen/controllers/toolControllers.js
+++ Projects/OpenKnesset/knest-rema-hosen/controllers/toolControllers.js
@@ -1,8 +1,35 @@
-let TOOL = {title:'כונוס הכנסת בזמן הפגרה',text:'הגשת דרישה ל יו"ר הכנסת על ידי 25 ח"כיים לכינוס  מליאה מיוחדת שתדון ב הצעה לסדר היום.'};
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://rema06:6m30hsL0v0jsdnXh@cluster0.eua4t.mongodb.net/open-knest', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',  ()=> {
+   console.log('we are connected to DB')
+});
+
+
+const ToolSchema = new mongoose.Schema({
+    title: String,
+    text: String
+  }); 
+  
+  const tool = mongoose.model('ToolSchema', ToolSchema); 
+
+//   const toolItem = new tool({ title: 'כונוס הכנסת בזמן הפגרה',text:'הגשת דרישה ל יו"ר הכנסת על ידי 25 ח"כיים לכינוס  מליאה מיוחדת שתדון ב הצעה לסדר היום.'});
+//   toolItem.save().then(()=>console.log('saved to DB'));
+
+// let TOOL = {title:'כונוס הכנסת בזמן הפגרה',text:'הגשת דרישה ל יו"ר הכנסת על ידי 25 ח"כיים לכינוס  מליאה מיוחדת שתדון ב הצעה לסדר היום.'};
 
 // handlers
   exports.getTOOL = (req, res) => {
-    res.send({ TOOL: TOOL});
+    try {
+      //get from DB
+      tool.find({}).then((TOOL) => {
+        res.send({ TOOL: TOOL});
+      });
+    } catch (e) {
+      res.send({ error: e });
+    }
    };
 
   exports.getTOOLById = (req, res) => {
