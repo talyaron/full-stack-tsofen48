dbafb0f234325348eb4a478ec6ec227ca313ca85
Ibrahim Lahham <abo.la7am.10@gmail.com>
2021-02-04 01:39:54 +0200

    Removing Array

--- Projects/OpenKnesset/Alaa - Ibrahim Lahham/parliamentary-tools/controllers/parliamentaryTools.js
+++ Projects/OpenKnesset/Alaa - Ibrahim Lahham/parliamentary-tools/controllers/parliamentaryTools.js
@@ -16,27 +16,14 @@ const Kittyschema = new mongoose.Schema({
 });
 
 
-const Kitten = mongoose.model('kittyschema', Kittyschema);
-const parliamentaryTools = [
-    {
-     title: "שאילה רגילה",
-      content: "שאלה לשר על עניין שהתחום תפקדיו ,אותה מפנה חבר כנסת , שאינו שר או סגן שר . התשובה לשאילתה תינתן במליאת הכנסת . על השר להשיב תוך 21 ימים ."
-    },
-    {
-      title: "נאום בן דקה",
-      content: "נאום של חבר כנסת , בכל נושא ובאורך של כדקה (תלוי בטוב ליבו של יו'ר הכנסת או סגן יו'ר הכנסת המנהלים את הישיבה בפועל)"
-    },
-    {
-      title: "כינוס הכנסת בזמן הפגרה",
-      content: "הגשת דרישה ליו'ר הכנסת על ידי 25 ח'כים לכינוס מליאה מיוחדת שתדון בהצעה לסדר היום"
-    }
-  ];
+const ParliamentaryTools = mongoose.model('kittyschema', Kittyschema);
+
 
   exports.getParliamentaryTools =  (req, res) => {
     console.log("in controller!!!!")
     try {
       //get from DB
-      Kitten.find({}).then(docs => {
+      ParliamentaryTools.find({}).then(docs => {
 
     //    console.log( { kittens: docs });
           res.send( { success:true,err:"not found!",parliamentaryTools: docs });
