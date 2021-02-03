6255441a10743aaaf35e23c3384d4c8e4e4e6fb2
يارا كبها <yara.kbha4@gmail.com>
2021-02-03 17:06:28 +0200

    yara sprint 4

---
+++ Projects/OpenPensia/sprint4/yara/server.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app = express();
+var cookieParser = require('cookie-parser');
+const voting = require('./routs/vote');
+
+app.use(cookieParser())
+
+app.use(express.static('rct/build'));
+app.use('/vote', voting)
+
+
+
+ 
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`\x1b[36m Server running on port ${port}  🔥`) });
+
+
+
