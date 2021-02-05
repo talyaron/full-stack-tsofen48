98dbad181732c2b1d593759e08bda549f971536b
Hosen <hosen.agbaria@gmail.com>
2021-02-05 12:11:17 +0200

    login controler and route

--- full-stack/shop/Hosen/client/src/App.js
+++ full-stack/shop/Hosen/client/src/App.js
@@ -1,53 +1,52 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 //components
-import Screen from './components/screen.js';
-
-
+import Screen from "./components/screen.js";
 
 function App() {
-  
-
   const [companies, setCompanies] = useState([]);
+  async function fetchMoviesBadStatus(url) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+
+    const movies = await response.json();
+    return movies;
+  }
 
   useEffect(() => {
-    fetch('/get-companies')
-      .then(r => r.json())
-      .then(data => {
-        console.log(data)
+   fetchMoviesBadStatus("/get-companies").then(res=>console.log('asdasd',res)).catch(error => {
+     console.log(error.message);
+    // error.message; // 'An error has occurred: 404'
+  });
+    fetch("/get-companies")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
         setCompanies(data.companies);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
-    
- //   <div></div>
+    //   <div></div>
 
-    
-    
-      <div >
-        
-        {companies.map((list, index) => {
-          return (<Screen
+    <div>
+      {companies.map((list, index) => {
+        return (
+          <Screen
             key={index}
             logo={list.logo}
             name={list.name}
             par={list.par}
-          />)
-
-
-        })}
-
+          />
+        );
+      })}
     </div>
-
-
-
-    
-
-
   );
-  
 }
 
 export default App;
