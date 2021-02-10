d99623dffbb09ba10012895d9d5c41a1a1dd6bed
mhmd kilaney <mhmdmhmood1937@gmail.com>
2021-02-10 18:40:05 +0200

    kilaney khalel

--- Projects/OpenPensia/Sprint5/khalil-kilaney/client/src/components/blocks/block.js
+++ Projects/OpenPensia/Sprint5/khalil-kilaney/client/src/components/blocks/block.js
@@ -5,7 +5,7 @@ function block({title , subtitle , url}) {
     <div className="block">
         <h3 className="title">{title}</h3>
         <span className="text">{subtitle}</span><br />
-        <a href={url} className="link">צפייה בכתבה</a>
+        <a href={url} className="link">לכתבה לחץ כאן</a>
     </div>
     );
 }
