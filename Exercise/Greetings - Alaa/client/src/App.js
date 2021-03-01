610266a5d5a4413655ecdb4c2ecbf741a11488aa
Alaa-S-M <alaasaabni@gmail.com>
2021-03-01 08:54:42 +0200

    Test3 - init new project from template

---
+++ Exercise/Greetings - Alaa/client/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+
+  function toggleClassName(e) {
+    if (e.target.tagName.toLowerCase() != 'a') {
+      e.target = e.target.parentNode;
+    }
+    let allTabs = document.querySelectorAll('nav>ul>a');
+    allTabs.forEach(tab => tab.classList.remove('active-nav'));
+    e.target.classList.add('active-nav');
+  }
+
+  return (
+    <Router>
+      <nav className='all-tabs' >
+        <ul onClick={toggleClassName}>
+          <Link className='nav active-nav' to="/">
+            <li>
+              Page 1
+          </li>
+          </Link>
+          <Link className='nav' to="#">
+            <li>
+              Page 2
+            </li>
+          </Link>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/test">
+
+        </Route>
+        <Route path="/">
+
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+
+
+
---
+++ Exercise/Greetings - Alaa/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://alaa:Hqd4WuQHYcpIfsLszXVr@cluster0.wvoxj.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const cookieParser = require('cookie-parser');
+// applicationCache.use(cookieParser);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('we are connected to DB')
+});
+
+
+
+// Server run
+const port = process.env.PORT || 3001;
+app.listen(port, () => { console.log(`\x1b[36m Server running on port ${port}  🔥`) });
